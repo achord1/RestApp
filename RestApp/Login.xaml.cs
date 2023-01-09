3ea72e0d121905ba36b98260ed6d9e7ca4dbae97
Ryan Barker <nztw247t2@vossie.net>
2023-01-09 22:01:28 +0200

    First Commit

---
+++ RestApp/Login.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RestApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Logg(object sender, EventArgs e)
+        {
+
+            string sUsername = entUsername.Text;
+            string sPassword = entPassword.Text;
+
+            if (sUsername == "User" && sPassword == "Pass") {
+                App.Current.MainPage = new purchase();
+            }
+        }
+    }
+}
---
+++ RestApp/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RestApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await Task.Delay(2000);
+            Application.Current.MainPage = new Login();
+        }
+    }
+}
